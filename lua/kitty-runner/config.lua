ab0f7420888d6483a167f96851ba7b7869899a84
lolpie244 <lolpie2651@gmail.com>
2023-06-29 14:36:02 +0300

    Remove unused env

--- lua/kitty-runner/config.lua
+++ lua/kitty-runner/config.lua
@@ -6,12 +6,12 @@ config.options = {
 		delay = 0,             -- delay between opening runner and sending command
 		extra_open_runner_args = {}, -- extra arguments for opening runner, Docs: https://sw.kovidgoyal.net/kitty/remote-control/#id14
 		extra_send_command_args = {}, -- extra arguments for sending command to runner, docs: https://sw.kovidgoyal.net/kitty/remote-control/#id22
-		env_to_copy = { "PATH", "VIRTUAL_ENV" }
+		env_to_copy = { "PATH" }
 	},
 	launch = {
 		default_location = "hsplit", -- default location of launch result. Docs: https://sw.kovidgoyal.net/kitty/remote-control/#cmdoption-kitty-launch-location
 		extra_launch_args = {}, -- extra arguments for launching to runner. Docs: https://sw.kovidgoyal.net/kitty/remote-control/#cmdoption-kitty-launch-location
-		env_to_copy = { "PATH", "VIRTUAL_ENV" },
+		env_to_copy = { "PATH" },
 	}
 }
 
